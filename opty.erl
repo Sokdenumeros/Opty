4061986d5bafab43a929d4fed76d4324bd78a37c
Philipp Panzer <philipp.panzer@fau.de>
2022-10-17 16:31:14 +0200

    start server on remote node

--- opty.erl
+++ opty.erl
@@ -1,6 +1,10 @@
 -module(opty).
 -export([start/5, stop/1]).
 
+-define(opty_server_node, 'opty-srv@127.0.0.1').
+-define(opty_server_regname, 's').
+-define(opty_server_tuple, {?opty_server_regname, ?opty_server_node}).
+
 %% Clients: Number of concurrent clients in the system
 %% Entries: Number of entries in the store
 %% Reads: Number of read operations per transaction
@@ -8,7 +12,9 @@
 %% Time: Duration of the experiment (in secs)
 
 start(Clients, Entries, Reads, Writes, Time) ->
-    register(s, server:start(Entries)),
+    % instead of registering the process locally, we spawn a process on the server node
+    % that spawns and registers the server there (under the name s)
+    spawn(?opty_server_node, fun() -> register(?opty_server_regname, server:start(Entries)) end),
     L = startClients(Clients, [], Entries, Reads, Writes),
     io:format("Starting: ~w CLIENTS, ~w ENTRIES, ~w RDxTR, ~w WRxTR, DURATION ~w s~n", 
          [Clients, Entries, Reads, Writes, Time]),
@@ -19,12 +25,12 @@ stop(L) ->
     io:format("Stopping...~n"),
     stopClients(L),
     waitClients(L),
-    s ! stop,
+    ?opty_server_tuple ! stop,
     io:format("Stopped~n").
 
 startClients(0, L, _, _, _) -> L;
 startClients(Clients, L, Entries, Reads, Writes) ->
-    Pid = client:start(Clients, Entries, Reads, Writes, s),
+    Pid = client:start(Clients, Entries, Reads, Writes, ?opty_server_tuple),
     startClients(Clients-1, [Pid|L], Entries, Reads, Writes).
 
 stopClients([]) ->
